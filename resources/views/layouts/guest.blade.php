8d790a90d6504241cf94f3ab347c4ff0f87bd60b
@gritla <lars.grit@hva.nl>
2025-04-11 12:30:58 +0200

    UUUGHHGGHGH part 5

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Beertracker') }}</title>
+    <title> Beertracker | Easy Beer Cost Tracking with Friends </title>
 
     <!-- Primary Meta Tags -->
     <meta name="title" content="Beertracker | Easy Beer Cost Tracking with Friends">
